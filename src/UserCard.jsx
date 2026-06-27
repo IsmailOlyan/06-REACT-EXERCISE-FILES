3b5c30544cb3fd7f69eae833c425c3ce718cbe2b
IsmailOlyan <ismaaciil1432@gmail.com>
2026-06-27 08:34:52 -0700

    worked exercise files

---
+++ src/UserCard.jsx
@@ -0,0 +1,10 @@
+const UserCard = ()=>{
+    return (
+        <div>
+            <h1>Eng Ismail Olyan</h1>
+            <p>ismaaciil1432@gmail.com</p>
+        </div>
+    )
+}
+
+export default UserCard;
