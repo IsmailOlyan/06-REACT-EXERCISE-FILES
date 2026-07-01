ad7ce4cc6c2751e4e54f6881d9c0644a91275b94
IsmailOlyan <ismaaciil1432@gmail.com>
2026-07-01 17:57:34 +0300

    worked exercise files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 // import UserCard from "./UserCard";
 // import UserCard from './components/Exercise-01/UserCard';
 // import Layout from './Layout'
- import Blog from './components/Exercise-02/Blog';
+// import Blog from './components/Exercise-03/UserCard';
 
+import UserCard from './components/Exercise-03/UserCard';
 function App(){
   return (
     <>
       {/* < UserCard/> */}
       {/* <Layout /> */}
-      <Blog />
+      {/* <Blog /> */}
+
+
+      <UserCard
+        name="Eng Ismail Olyan"
+        email="ismaaciil1432@gmail.com"
+      />
+      <UserCard
+        name="Eng Abdirahman Alasow"
+        email="alasow@gmail.com"
+      />
+      <UserCard
+        name="Eng Omar Abdi"
+        email="mromarabdi@gmail.com"
+      />
+
     </>
   )
 }
---
+++ src/Components/Exercise-03/UserCard.jsx
@@ -0,0 +1,10 @@
+const UserCard = ({name, email})=>{
+    return (
+        <div>
+            <h1>User's Name is {name}</h1>
+            <p> User's Email is {email}</p>
+        </div>
+    )
+}
+
+export default UserCard;
